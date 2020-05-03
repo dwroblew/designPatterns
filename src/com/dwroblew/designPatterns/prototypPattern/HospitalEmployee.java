f08f3ed15daad50b220228ad4d744e1011dc48aa
Damian Wroblewski <damian.wroblewski@sinovo.de>
2020-05-03 13:31:14 +0200

    pattern: prototyp

---
+++ src/com/dwroblew/designPatterns/prototypPattern/HospitalEmployee.java
@@ -0,0 +1,34 @@
+package com.dwroblew.designPatterns.prototypPattern;
+
+public class HospitalEmployee extends Employee
+{
+   private String position;
+
+   public HospitalEmployee( String firstName, String lastName, Address address, String position )
+   {
+      super( firstName, lastName, address );
+      this.position = position;
+   }
+
+   public String getPosition()
+   {
+      return position;
+   }
+
+   public void setPosition( String position )
+   {
+      this.position = position;
+   }
+
+   @Override
+   public String toString()
+   {
+      return super.toString() + "[position='" + position + "]";
+   }
+
+   @Override
+   public HospitalEmployee clone()
+   {
+      return (HospitalEmployee) super.clone();
+   }
+}
